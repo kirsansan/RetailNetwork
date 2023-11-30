34abba34ba3d388987430f277725f79d8d0d01fc
kirsan <kirill_home@mail.ru>
2023-11-30 19:26:26 +0300

    all comments.docx (copy as comments.pdf)

--- base/validators_model.py
+++ base/validators_model.py
@@ -1,30 +1,14 @@
 from rest_framework.exceptions import ValidationError
-from config.config import HIERARCHY_MODE
 
 
 def validate_complex_case(instance):
-    if instance.node_type != 0:  # non factory
-        # check links and their types
-        if instance.supplier_link is not None:
+    """Validate fields of Model NetworkNode before saving"""
+    if instance.supplier_link is not None:
+        if instance.node_type == 0:
+            raise ValidationError('Factory cannot have reference')
+        else:  # non-factory
             if instance.supplier_link.node_type > instance.node_type:
-                message = "You cannot reference a node with a higher hierarchy level"
-                raise ValidationError({'__all__': [message]})
-        else:
-            message = "You must reference to supplier"
-            raise ValidationError({'__all__': [message]})
-        # Hard checking if HIERARCHY_MODE
-        if HIERARCHY_MODE:
-            pass
-            # if instance.node_type == 2 \
-            #         and instance.factory_link is not None:
-            #     message = "IndividualEntrepreneur can work only with RetailNetworks"
-            #     raise ValidationError({'__all__': [message]})
-            # if instance.node_type == 1 \
-            #         and instance.retail_network_link is not None:
-            #     message = "RetailNetwork can work only with Factories"
-            #     raise ValidationError({'__all__': [message]})
-
-    else:  # Factory
-        if instance.supplier_link is not None:
-            message = "Factory mustn't have supplier links"
-            raise ValidationError({'__all__': [message]})
+                raise ValidationError('You cannot reference a node with a higher hierarchy level')
+    else:
+        if instance.node_type != 0:
+            raise ValidationError('Non Factory object must have a reference')
--- config/config.py
+++ config/config.py
@@ -2,25 +2,9 @@ import os
 # install as python-dotenv
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-
-
-
-# for future in case of notices via e-mail
-# MY_EMAIL = os.getenv('MY_EMAIL')
-# MY_EMAIL_PASSWORD = os.getenv('MY_EMAIL_PASSWORD')
-# MY_EMAIL_HOST = os.getenv('MY_EMAIL_HOST')
-# MY_EMAIL_PORT = int(os.getenv('MY_EMAIL_PORT'))
-# if os.getenv('EMAIL_SENDING_SIMULATION_MODE') == 'False':
-#     EMAIL_SENDING_SIMULATION_MODE = False
-# else:
-#     EMAIL_SENDING_SIMULATION_MODE = True
-
-
 DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-
 
 ENV_TYPE = os.getenv('ENV_TYPE')
 
@@ -45,9 +29,4 @@ else:
 
 LOG_FILE_NAME = "retailnetwork_log.log"
 
-
 MAX_NODE_PER_PAGE = 10
-
-HIERARCHY_MODE = True
-# in this mode only Factory-type node might be available as Supplier for RetailNetwork node
-# and only RetailNetwork-type node might be available as Supplier for IndividualEntrepreneur
